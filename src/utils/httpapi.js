48ecfdbbc52d27ae185cf8a26bf41047aefb91c8
Hal Blackburn <hwtb2@cam.ac.uk>
2016-11-30 18:28:52 +0000

    Ensure concurrent token requests are not made
    If a second request for a token is made while one is already active
    then the existing request is re-used instead of creating a new one.

--- src/utils/httpapi.js
+++ src/utils/httpapi.js
@@ -13,18 +13,40 @@ const TOKEN_REQUEST_TIMEOUT = 15 * 1000;
 
 export class LoginRequiredError extends ExtendableError { }
 
+const DEFAULT_ACTIVE_REQUESTS = {};
+const DEFAULT_OPTIONS = {
+    cache: true,
+    activeRequests: DEFAULT_ACTIVE_REQUESTS
+};
+
 class AuthTokenSource {
     constructor(audienceUrl, options) {
         options = assign({}, DEFAULT_OPTIONS, options);
 
         this._audienceUrl = audienceUrl;
         this._cache = !!options.cache;
+        this._activeRequests = options.activeRequests;
     }
 
     getAuthToken() {
         let token = this._cache ? this._getCachedToken() : null;
 
-        return Q(token || this._obtainNewToken());
+        if(token)
+            return Q(token);
+
+        let cacheKey = this._tokenCacheKey();
+
+        // Don't start a concurrent request if one exists.
+        if(this._activeRequests[cacheKey])
+            return this._activeRequests[cacheKey];
+
+        let promise = this._activeRequests[cacheKey] = this._obtainNewToken()
+        .finally(() => {
+            if(this._activeRequests[cacheKey] === promise)
+                delete this._activeRequests[cacheKey];
+        });
+
+        return promise;
     }
 
     _appearsValid(jwtString) {
@@ -71,10 +93,6 @@ class AuthTokenSource {
         .tap(token => sessionStorage.setItem(this._tokenCacheKey(), token));
     }
 }
-
-const DEFAULT_OPTIONS = {
-    cache: true
-};
 
 /**
  * Get a promise for an authentication token for the specified audience URI.
